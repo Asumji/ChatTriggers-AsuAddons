d14efb7dfa0ffb142426f243a67dbe150bab62e6
Asumji <64544378+Asumji@users.noreply.github.com>
2023-01-26 16:33:00 +0100

    Update index.js

--- AsuAddons/index.js
+++ AsuAddons/index.js
@@ -3,16 +3,21 @@
 import PogObject from "PogData";
 
 const data = new PogObject("AsuAddons", {
-  names: ["weeklies"],
-  owner: "Party > [MVP+] weeklies",
-  bot: "RPZ2"
+  FRAGnames: ["weeklies"],
+  FRAGowner: "Party > [MVP+] weeklies",
+  FRAGbot: "RPZ2",
+  FRAGenabled: true,
+  apiKey: "",
+  DPUrelevantItems: ["scylla", "hyperion", "astraea", "valkyrie", "terminator", "juju", "axe of the shredded", "livid dagger", "spirit bow", "last breath"],
+  DPUenabled: true
 });
 data.save();
 
-export { data }
-
 const File = Java.type("java.io.File")
 const f = new File("config/ChatTriggers/modules/AsuAddons/", "mods")
+const modPrefix = "&6AU > &r"
+
+export { data, modPrefix }
 
 if (f.exists()) {
     const fileArray = f.listFiles()
