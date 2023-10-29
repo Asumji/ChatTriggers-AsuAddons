67b7d2719026115940baa2c6ef957072d294e3d2
Asumji <deralleshaterhi@gmail.com>
2023-10-29 15:58:09 +0100

    stuff

--- mods/apiMonitor.js
+++ mods/apiMonitor.js
@@ -11,6 +11,7 @@ const stepRegister = register("step", () => {
         getrequest("http://asumji.duckdns.org:3000/test").then(() => {
             console.log("AU > API Check succeeded everything is working properly!")
         }).catch(function(error) {
+            if (error != "JavaException: java.net.ConnectException: Connection timed out: connect") return
             print("AU > API Check failed!\n" + error)
             ChatLib.chat(new Message(modPrefix + " §cThe API cannot be reached! Some features might not work.\n").addTextComponent(new TextComponent("§a§l[CLICK HERE]§r§a to send a report to the dev.").setClick("run_command", "/au report " + error)))
             stepRegister.unregister()
