a23cfca987b337d89b58afe5fe248b7887b6106c
Asumji <deralleshaterhi@gmail.com>
2025-03-25 16:45:35 +0100

    e :3

--- mods/witherHitbox.js
+++ mods/witherHitbox.js
@@ -3,7 +3,7 @@ import { drawBox, drawInnerBox } from "../utils.js"
 
 register("postRenderEntity", (entity) => {
     if (entity.getClassName() != "EntityArmorStand" || !data.wither.hitbox) return
-    if (!entity.getName().match(/Maxor|Storm|Goldor|Necron/)) return
+    if (!entity.getName().removeFormatting().match(/Maxor |Storm |Goldor |Necron /)) return
     data.wither.type == 0 ? 
         drawBox(entity.x,entity.y,entity.z,1,-4,Number(data.wither.color[0])/100,Number(data.wither.color[1])/100,Number(data.wither.color[2])/100,1,false) :   
         drawInnerBox(entity.x,entity.y,entity.z,1,-4,Number(data.wither.color[0])/100,Number(data.wither.color[1])/100,Number(data.wither.color[2])/100,1,false)
